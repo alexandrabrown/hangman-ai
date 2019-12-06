22010e099fb069a072f1592596625e03ad8cac0c
Alexandra Brown <alexbro@umich.edu>
2019-12-06 13:07:34 -0500

    Add candidateSetFns

--- hangman.js
+++ hangman.js
@@ -1,20 +1,41 @@
-var numLetters = 0;
+var dictionary = [];
+var candidateSet = [];
+var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+var knownLetters = ['e' => [0, 2], 'l' => [1] ]; //elephant
 
 $(document).ready(function() {
     storeDictionary();
 });
 
-function readFile(file) {
-    $.get(file, function(data) {
-        console.log( "Data Loaded: " + data );
-    });
-}
-
 function storeDictionary() {
     readFile('words_alpha.txt');
 }
 
-function chooseWordLength(input) {
-    numLetters = input;
+function readFile(file) {
+    $.get(file, function(data) {
+        dictionary = data.split("\n");
+    });
+}
+
+function candidateSet(wordLength) {
+    for (word in dictionary) {
+        if (word == wordLength) {
+            candidateSet.push(word);
+        }
+    }
+}
+
+function generateNewCandidateSet(knownLetters) {
+    for (letter in knownLetters) {
+        for (indices in knownLetters[letter]) {
+
+        }
+
+    }
+    for (word in candidateSet) {
+        for (letter in word) {
+
+        }
+    }
 }
 
